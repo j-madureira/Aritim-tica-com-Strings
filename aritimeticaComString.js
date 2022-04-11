85c4773ff58939554f766bde3ac13e8a3948eba4
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-11 15:57:34 -0300

    Add files via upload

---
+++ aritimeticaComString.js
@@ -0,0 +1,13 @@
+/*Criar uma variável do tipo string ela terá um valor que é "8"
+  Fazer operações aritiméticas a partir dela
+*/
+
+//Multiplicação utilizando uma String
+var dado = "8";
+var continha = dado * 3;
+console.log("A multiplicação entre ", dado, "e", 3, "é:", continha);
+
+//Divisão utilizando uma String
+var dadoDois = "15";
+var continhaDois = dadoDois / 5;
+console.log("A divisão de", dadoDois, "e", 5, "é:", continhaDois);
